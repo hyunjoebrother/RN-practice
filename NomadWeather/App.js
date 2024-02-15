272229a36b5a70d60e8c621aa0d4d722cde7705c
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-02-15 21:39:36 +0900

    feat: add expo-location API

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,19 +1,63 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, Vibration } from "react-native";
+import { useEffect, useState } from "react";
+import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
+import * as Location from "expo-location";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const [city, setCity] = useState("Loading...")
+
+  const getPermmisions = async () => {
+    // 권한 요청 - 거부 시
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+
+    // 현재 위치
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city)
+  };
+
+  useEffect(() => {
+    getPermmisions();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text
-        style={{
-          fontSize: 12,
-          color: "red",
-        }}
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
       >
-        Open up App.js
-      </Text>
-      <StatusBar style="light" />
-      <Button onPress={() => Vibration.vibrate(PATTERN, true)}>진동</Button>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>17</Text>
+          <Text style={styles.description}>Rainy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>-7</Text>
+          <Text style={styles.description}>Windy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>0</Text>
+          <Text style={styles.description}>Snowy</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -21,11 +65,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "tomato",
   },
-  text: {
-    fontSize: 28,
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 48,
+    fontWeight: "500",
+  },
+  weather: {
+    // flex: 3,
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 168,
+    marginTop: 50,
+  },
+  description: {
+    marginTop: -24,
+    fontSize: 48,
   },
 });
