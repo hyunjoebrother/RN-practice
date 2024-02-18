9e1c1e6969801af66d3e081a2417a8928a9a374a
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-02-18 11:59:14 +0900

    about TextInput Component

--- WorkHardTravelHardApp/App.js
+++ WorkHardTravelHardApp/App.js
@@ -4,26 +4,48 @@ import {
   Text,
   View,
   TouchableOpacity,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
+  TextInput,
 } from "react-native";
 import { theme } from "./colors";
+import { useState } from "react";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        {/* <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
-        </TouchableOpacity> */}
-        <TouchableWithoutFeedback onPress={() => console.log("pressed!!")}>
-          <Text style={styles.btnText}>Work</Text>
-        </TouchableWithoutFeedback>
-        <TouchableHighlight underlayColor={"red"} activeOpacity={0.2}>
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableHighlight>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: working ? "white" : theme.grey,
+            }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
       </View>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        placeholder={working ? "Add a TODO" : "Whrere do u wanna go?"}
+      />
     </View>
   );
 }
@@ -42,6 +64,13 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 36,
     fontWeight: "600",
-    color: theme.white,
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
--- WorkHardTravelHardApp/colors.js
+++ WorkHardTravelHardApp/colors.js
@@ -1,5 +1,4 @@
 export const theme = {
   bg: "black",
   grey: "#3A3D40",
-  white: "#fff",
 };
